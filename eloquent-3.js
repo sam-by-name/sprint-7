dc4dd4ef13600f2f8ea9e31b4c9487bd7f9e7637
sam-by-name <sam-sanders@hotmail.co.uk>
2018-04-30 16:59:26 +1200

    eloquent explained not so eloquently :(

--- eloquent-3.js
+++ eloquent-3.js
@@ -17,3 +17,19 @@ console.log(min(0, 10));
 // → 0
 console.log(min(0, -10));
 // → -10
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function countBs(string) {
+    let count = 0;
+    for (let i = 0; i < string.length; i++) {
+      if (string[i] == "B") {
+      count ++;
+      }
+    }
+    return count;
+  }
+  console.log(countBs("BBC"));
+
